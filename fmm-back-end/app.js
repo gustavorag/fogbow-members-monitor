61dd9b226bfda0ee34d932a5d740517888717d0f
gustavorag <gustavorag@gmail.com>
2017-04-02 18:52:46 -0700

    fixies

--- fmm-back-end/app.js
+++ fmm-back-end/app.js
@@ -10,14 +10,14 @@ var port = 8800;
 
 var membersDetail;
 
-var baseLdap="dc=lsd,dc=ufcg,dc=edu,dc=br";
+var baseLdap="base";
 var encrypt="";
 var privateKey="";
 var publicKey="";
-var username="fogbowadm";
-var password="f0gb0w4dm";
-var authUrl="ldap://ldap.lsd.ufcg.edu.br:389";
-var managerUrl="http://10.11.4.234:8182"
+var username="use";
+var password="pass";
+var authUrl="ldap://host:post";
+var managerUrl="http://localhost:8182"
 
 
 var baseDiretory="";
@@ -28,7 +28,6 @@ var createTokenBase=" token --create --type ldap ";
 
 var CLI_BASE_COMMAND = "";
 var CLI_CREATE_TOKEN = "";
-
 
 var createTokenFunc = function(startApiFunc){
 
@@ -67,7 +66,6 @@ var initialConfiguration = function(){
 		CLI_BASE_COMMAND = "java -cp "+baseDiretory+"/fogbow-components/fogbow-cli-0.0.1-SNAPSHOT-jar-with-dependencies.jar org.fogbowcloud.cli.Main"
 		CLI_CREATE_TOKEN = CLI_BASE_COMMAND+createTokenBase+"-Dbase="+baseLdap+" -Dencrypt= -DprivateKey="+privateKey+" -DpublicKey="+publicKey+" -Dusername="+username+" -Dpassword="+password+" -DauthUrl="+authUrl
 		
-	  	//createTokenFunc(startApi);
 	  	startApp();
 	  }
 	});
@@ -75,7 +73,7 @@ var initialConfiguration = function(){
 
 
 var extractMemberDetail = function(details){
-	
+	console.log("extracting details: "+details)
 	var detailsJson = {};
 	// Format: cpuQuota=10;cpuInUse=6;cpuInUseByUser=4;memQuota=1024;memInUse=512;memInUseByUser=512;instancesQuota=10;instancesInUse=3;instancesInUseByUser=1
 	var itens = details.split(';');
@@ -93,11 +91,24 @@ var extractMemberDetail = function(details){
 }
 
 var processMemberDetail = function(member, details){
-
+	console.log("Processing Member details: "+member+" \n "+details)
 	//Calculate Usages in %
-	member.memUsage = ((details.memInUse * 100) / details.memQuota).toFixed(2);
-	member.cpuUsage = ((details.cpuInUse * 100) / details.cpuQuota).toFixed(2);
-	member.instanceUsage = ((details.instancesInUse * 100) / details.instancesQuota).toFixed(2);
+	if(details.memInUse > 0){
+		member.memUsage = ((details.memInUse * 100) / details.memQuota).toFixed(2);	
+	}else{
+		member.memUsage = 0;
+	}
+	if(details.cpuInUse > 0){
+		member.cpuUsage = ((details.cpuInUse * 100) / details.cpuQuota).toFixed(2);
+	}else{
+		member.cpuUsage = 0;
+	}
+	if(details.instancesInUse > 0){
+		member.instanceUsage = ((details.instancesInUse * 100) / details.instancesQuota).toFixed(2);
+	}else{
+		member.instanceUsage = 0;
+	}
+	
 	member.instancesInUse = details.instancesInUse;
 	member.intancesQuota = details.instancesQuota;
 
@@ -112,45 +123,45 @@ function startApp(){
 
 			var getMemberDetail = function(req, res, members){
 				
-
-				//console.log("Executing: "+CLI_GET_MEMBERS);
-				// var childTest = exec(CLI_GET_MEMBER_DETAIL, function (error, stdout, stderr) {
-				// 	console.log("Stdout: "+stdout);
-				// 	console.log("Stderr: "+stderr);
-				// 	if (error !== null) {
-				// 		console.log('exec error: ' + error);
-				// 	}else{
-				// 		return stdout.split(/(\r\n|\n|\r)/gm);
-				// 	}
-				// });
-
+				var membersErrorCount = 0;
 				var membersJson = [];
-				//console.log("Members: "+JSON.stringify(members));
-				if(Array.isArray(members)){
-					
-					members.forEach(function(item, index){
-						if(item){
-							var getMemberDetailCli=" member --url "+managerUrl+" --quota --id "+item+" --auth-token "+token;
-							var CLI_GET_MEMBER_DETAIL = CLI_BASE_COMMAND+getMemberDetailCli;
-							
-							console.log("Executing: " CLI_GET_MEMBER_DETAIL);
+				var membersValidate = [];
 
-							var newMember = {
-								name:item,
-								memUsage:0,
-								cpuUsage:0,
-								instanceUsage:0,
-								instancesInUse:0,
-								intancesQuota:0
-							}
-							var childTest = exec(CLI_GET_MEMBER_DETAIL, function (error, stdout, stderr) {
-								//console.log("Stdout: "+stdout);
-								//console.log("Stderr: "+stderr);
-								if (error !== null) {
-									console.log('exec error: ' + error);
-								}else{
-									
-									var detailsString = stdout.replace(/(\r\n|\n|\r)/gm, ';');
+				if(Array.isArray(members)){
+
+					members.forEach(function(item, index){
+						console.log('Verificando '+item)
+						if(item){
+							membersValidate.push(item);
+						}
+					});
+					console.log('Members Validate: '+membersValidate);
+					membersValidate.forEach(function(item, index){
+						
+						var getMemberDetailCli=" member --url "+managerUrl+" --quota --id "+item+" --auth-token "+token;
+						var CLI_GET_MEMBER_DETAIL = CLI_BASE_COMMAND+getMemberDetailCli;
+						
+						//console.log("Executing: "+CLI_GET_MEMBER_DETAIL);
+
+						var newMember = {
+							name:item,
+							memUsage:0,
+							cpuUsage:0,
+							instanceUsage:0,
+							instancesInUse:0,
+							intancesQuota:0
+						}
+						var childTest = exec(CLI_GET_MEMBER_DETAIL, function (error, stdout, stderr) {
+							//console.log("Stdout: "+stdout);
+							//console.log("Stderr: "+stderr);
+							if (error !== null) {
+								membersErrorCount++;
+								console.log('exec error: ' + error);
+							}else{
+								
+								var detailsString = stdout.replace(/(\r\n|\n|\r)/gm, ';');
+								if(detailsString.match(/X-OCCI-Attribute: /)){
+
 									detailsString = detailsString.replace(/X-OCCI-Attribute: /g, '');
 									//console.log('Detalhes: '+JSON.stringify(detailsString));
 
@@ -159,40 +170,23 @@ function startApp(){
 									newMember = processMemberDetail(newMember, memberDetails);
 
 									membersJson.push(newMember);
-									
+								}else{
+									membersErrorCount++
 								}
-								if(index+1 == members.length){
-									res.send(membersJson);
-									res.end();
-								}
-							});
-							// var detailsString = "X-OCCI-Attribute: cpuQuota=60\n"+
-							// 					"X-OCCI-Attribute: cpuInUse=20\n"+
-							// 					"X-OCCI-Attribute: cpuInUseByUser=16\n"+
-							// 					"X-OCCI-Attribute: memQuota=77824\n"+
-							// 					"X-OCCI-Attribute: memInUse=65536\n"+
-							// 					"X-OCCI-Attribute: memInUseByUser=57344\n"+
-							// 					"X-OCCI-Attribute: instancesQuota=30\n"+
-							// 					"X-OCCI-Attribute: instancesInUse=6\n"+
-							// 					"X-OCCI-Attribute: instancesInUseByUser=2";
-
+							}
+							console.log("Members Cash Size "+membersJson.length);
+							console.log("Total valide responses "+(membersValidate.length - membersErrorCount))
+							//Make sure that the last response from fogbow will write the response for the frontend
+							if(membersJson.length >= (membersValidate.length - membersErrorCount)){
+								res.send(membersJson);
+								res.end();
+							}
 							
-							// detailsString = detailsString.replace(/(\r\n|\n|\r)/gm, ';');
-							// detailsString = detailsString.replace(/X-OCCI-Attribute: /g, '');
-							// //console.log('Detalhes: '+JSON.stringify(detailsString));
-
-							// var memberDetails = extractMemberDetail(detailsString);
-
-							// newMember = processMemberDetail(newMember, memberDetails);
-
-							// membersJson.push(newMember);
-						}
+						});
+						
 					});
 
 				}
-
-				// res.send(membersJson);
-				// res.end();				
 			}
 
 			var getMembersCli=" member --url "+managerUrl+" --auth-token "+token;
@@ -204,15 +198,11 @@ function startApp(){
 				if (error !== null) {
 					console.log('exec error: ' + error);
 				}else{
-					// return stdout.split(/(\r\n|\n|\r)/gm);
-					// var stdout = "catch-all.manager.naf.lsd.ufcg.edu.br\nfogbow-manager-rec.compute.rnp.br\nlsd.manager.naf.lsd.ufcg.edu.br\nmanager.naf.ufscar.br\ntu.dresden.manager.naf.lsd.ufcg.edu.br"
 					stdout = stdout.replace(/(\r\n|\n|\r)/gm, ';');
 					getMemberDetail(req, res, stdout.split(';'));
 				}
 			});
-			// var stdout = "catch-all.manager.naf.lsd.ufcg.edu.br\nfogbow-manager-rec.compute.rnp.br\nlsd.manager.naf.lsd.ufcg.edu.br\nmanager.naf.ufscar.br\ntu.dresden.manager.naf.lsd.ufcg.edu.br"
-			// stdout = stdout.replace(/(\r\n|\n|\r)/gm, ';');
-			// getMemberDetail(req, res, stdout.split(';'));
+
 			
 		}
 	};
@@ -233,30 +223,9 @@ function startApp(){
 
 	//Return index page.
 	app.get("/members", function(req, res) {
-
-		//console.log("Returning "+fogbowApi.getMembers());
 		fogbowApi.getMembers(req, res);
-	  //res.sendFile(path.join(__dirname + '/public/index.html'));
 	});
 
 }
 
 initialConfiguration();
-
-
-//Routing for api
-
-//app.use('/api', fogbowApi);
-
-// function validate(req, res, next){
-// 	console.log("Validating")
-// 	next();
-// }
-
-//Sampler for get
-// app.get("URL",
-// 	cors.initialize(),
-// 	auth.verify(),
-// 	function(req, res){
-
-// });
